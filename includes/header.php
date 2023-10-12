752ad3527a7a614088543fde99331d214bc78bcf
Talatizi <talatizikamel88@gmail.com>
2023-10-12 15:25:55 +0200

    Projet blog - version 3 - Authentification

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,21 @@
 <header>
     <a href="/" class="logo">Dyma Blog</a>
     <ul class="header-menu">
-        <li class=<?= $_SERVER['REQUEST_URI'] === '/form-article.php' ? 'active' : '' ?>>
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/auth-register.php' ? 'active' : '' ?>>
+            <a href="/auth-register.php">Inscription</a>
+            </li>
+            <li class=<?= $_SERVER['REQUEST_URI'] === '/auth-login.php' ? 'active' : '' ?>>
+            <a href="/auth-login.php">Connexion</a>
+            </li>
+            <li class=<?= $_SERVER['REQUEST_URI'] === '/auth-logout.php' ? 'active' : '' ?>>
+            <a href="/auth-logout.php">déconnexion</a>
+            </li>
+            <li class=<?= $_SERVER['REQUEST_URI'] === '/profile.php' ? 'active' : '' ?>>
+            <a href="/profile.php">profile</a>
+            </li>
+            <li class=<?= $_SERVER['REQUEST_URI'] === '/form-article.php' ? 'active' : '' ?>>
             <a href="/form-article.php">Écrire un article</a>
-        </li>
+            </li>
+
     </ul>
 </header>
